325270e4e0b0a7b1e60c3eda754560f729259948
Александр Данильченко <mlemix32@gmail.com>
2022-04-16 01:16:43 +0300

    Добавьте файлы проекта.

---
+++ calendar.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	system("color 0B");
+	setlocale(0, "rus");
+	cout << "\n\n\t\t\t\t\t 2022\n";
+	cout << "\t\t\t\t\tАпрель              \n\n";
+	cout << "\t\tПН\tВТ\tСР\tЧТ\tПТ\tСБ\tВС\n";
+
+	cout << "\t\t\t\t\t\t 1\t 2\t 3\n";
+	cout << "\t\t 4\t 5\t 6\t 7\t 8\t 9\t10\n";
+	cout << "\t\t11\t12\t13\t14\t15\t16\t17\n";
+	cout << "\t\t18\t19\t20\t21\t22\t23\t24\n";
+	cout << "\t\t25\t26\t27\t28\t29\t30\n\n";
+
+}
+
+
+
